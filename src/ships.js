19cf52bc2b26e27a272a05db7c9243dae6a3dd01
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2024-06-23 18:46:27 -0500

    Add ship class after testing ship.test.js

--- src/ships.js
+++ src/ships.js
@@ -1,8 +1,19 @@
-class Ship {
+export class Ship {
   constructor(name, length) {
     this.name = name;
     this.length = length;
   }
   damageTaken = 0;
-  isSunk = '';
+
+  hit() {
+    this.damageTaken++;
+  }
+
+  isSunk() {
+    if (this.damageTaken >= this.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
